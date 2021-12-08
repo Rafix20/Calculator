2cd2d4eaa21b2bb6055e4afdefec76db2529894b
Rafix <62878878+Rafix20@users.noreply.github.com>
2021-12-08 23:20:05 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    int a,b;
+
+    public Main(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Main(int a) {
+        this.a = a;
+    }
+
+    public int add() {
+        return a + b;
+    }
+
+    public int sub() {
+        return a - b;
+    }
+
+    public int multi() {
+        return a * b;
+    }
+
+    public int div() {
+        return a / b;
+    }
+
+    public double power(){
+        return Math.pow(a,b);
+    }
+
+    public double root(){
+        return Math.sqrt(a);
+    }
+
+}
