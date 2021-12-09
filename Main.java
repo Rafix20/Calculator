4542c3e2190b53c3fcba01ba18c18aeb62bb3c56
Rafix <62878878+Rafix20@users.noreply.github.com>
2021-12-09 14:43:38 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,27 +9,27 @@ public class Main {
     public Main(int a) {
         this.a = a;
     }
-
+    //dodawanie
     public int add() {
         return a + b;
     }
-
+    //odejmowanie
     public int sub() {
         return a - b;
     }
-
+    //mnożenie
     public int multi() {
         return a * b;
     }
-
+    //dzielenie
     public int div() {
         return a / b;
     }
-
+    //do potęgi
     public double power(){
         return Math.pow(a,b);
     }
-
+    //z pierwiastka kwadratowego
     public double square(){
         return Math.sqrt(a);
     }
